5fa4832ad01795cdd492dfe43df25b14744e6674
Roman <r.zubehin@gmail.com>
2020-10-23 22:13:39 +0300

    change path to file mainWindow.fxml

--- src/sample/model/Main.java
+++ src/sample/model/Main.java
@@ -6,11 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/sample/view/mainWindow.fxml"));
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/mainWindow.fxml"));
         primaryStage.setTitle("JavaFX App");
         primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
